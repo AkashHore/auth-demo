fb7c7efd2798509bdb4c1123991083260b080d7b
Akash Hore <akashhore94@gmail.com>
2026-09-03 17:38:43 +0530

    project init

---
+++ src/lib/env/serverEnv.ts
@@ -0,0 +1,20 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import z from "zod";
+
+export const serverEnv = createEnv({
+  server: {
+    DATABASE_URL: z
+      .string()
+      .startsWith("file:./", {
+        error: "DATABASE_URL must start with file:./",
+      })
+      .min(1, { error: "DATABASE_URL is required" }),
+    NEXT_TELEMETRY_DISABLED: z.enum(["1", "0"]).optional(),
+    CHECKPOINT_DISABLE: z.enum(["1", "0"]).optional(),
+    BETTER_AUTH_SECRET: z
+      .string()
+      .min(1, { error: "BETTER_AUTH_SECRET is required" }),
+    BETTER_AUTH_URL: z.url().min(1, { error: "BETTER_AUTH_URL is required" }),
+  },
+  experimental__runtimeEnv: process.env,
+});
